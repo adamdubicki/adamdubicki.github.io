1c5b30ce0212d0ce3920ba893f3f2e111ea34fd2
Adam Dubicki <adam.dubicki@awl4lg7t60g.celonis.cloud>
2026-02-12 13:49:33 -0800

    Correct Astro.config, remove unnecessary deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://adamdubicki.github.io
+    site: "https://adamdubicki.github.io"
 });
