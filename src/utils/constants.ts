6197e304d08c909f6d395946fe9be0a7b18aedf9
Adam Dubicki <adamdubicki@outlook.com>
2026-02-17 15:57:52 -0800

    Formatting, will add commit hook sigh.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -14,6 +14,7 @@ export const NAVIGATION = [
   { href: "#experience", text: "Experience" },
   { href: "/#contact", text: "Contact" },
   {
-    href: "/books-i-have-read", "text": "Books I have read"
-  }
+    href: "/books-i-have-read",
+    text: "Books I have read",
+  },
 ];
